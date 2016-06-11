565f28b0fd4bf2c3d8196caa7ce698256860d2a6
Samuel .H <samuelpccl@gmail.com>
2016-06-11 15:39:59 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+		<div class="centro">	
+		<form method="post" action="config.php">
+			<p><input type="text" name="user" placeholder="Usuario"class="tammy"></p>
+			<p><input type="password" name="pass" placeholder="Senha"class="tammy"></p>
+			<p><input type="submit" name="confirma" class="botao"></p>	
+		</form>
+		</div>
+</body>
+</html>
